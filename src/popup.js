aa702147ca127a2d4c019131240b20e3572b6849
PretzelSmoothie <Con_Rice@Connors-MacBook-Pro.local>
2016-12-03 15:34:11 -0800

    latest update

--- src/popup.js
+++ src/popup.js
@@ -3,16 +3,24 @@
 // found in the LICENSE file.
 
 document.addEventListener('DOMContentLoaded', onInit, false);
-var output;
+var update;
+var updateMe;
+var myUrl = "";
 function onInit(){
   alert("running");
-  output = document.getElementById("test");
+  update = document.getElementById("update");
+  updateMe = document.getElementById("updateMe");
 
-  output.addEventListener('click', function() {
-    alert("made it!");
+  /*update.addEventListener('click', function() {
     getCurrentTabUrl(function(url) {
-
+      //set updateMe URL
+      updateMe = document.getElementById("linkToUpdate");
+      updateMe.setAttribute("href", url);
     });
+  });*/
+
+  update.addEventListener('click', function () {
+    getTab();
   });
 
 }
@@ -40,11 +48,7 @@ function getCurrentTabUrl(callback) {
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
+
     var tab = tabs[0];
 
     // A tab is a plain object that provides information about the tab.
@@ -56,9 +60,29 @@ function getCurrentTabUrl(callback) {
     // from |queryInfo|), then the "tabs" permission is required to see their
     // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
-    console.log(url);
+
+
 
     callback(url);
   });
-
   }
+
+
+function getTab() {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+  chrome.tabs.query(queryInfo, function(tabs) {
+    myUrl = tabs[0].url;
+    alert(myUrl);
+
+  });
+
+}
+
+
+
+//ADDITION FOR TESTING
+
+//when UPDATE is clicked, we want to change the url of UPDATEME
